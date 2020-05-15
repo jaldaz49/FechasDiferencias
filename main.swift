ca30aea95a31fc0e9a043f4284d0460fea53e269
jaldaz49 <65065322+jaldaz49@users.noreply.github.com>
2020-05-14 22:54:21 -0300

    Update main.swift
    Trabajo con fechas. Distintos ejemplos

--- main.swift
+++ main.swift
@@ -16,9 +16,7 @@ someDateComponentsFormatter.includesApproximationPhrase = true
 someDateComponentsFormatter.includesTimeRemainingPhrase = true
 someDateComponentsFormatter.allowedUnits = [.month, .day, .hour]
 
-Any futureDate = someDateComponents.date
-print(futureDate)
-let remainingTime = someDateComponentsFormatter.string(from: Date(), to: futureDate!)!
+DateComponentsFormatter.string(from: Date(), to: futureDate!)!
 
 let outputDateFormatter = DateFormatter()
 print(outputDateFormatter)
@@ -27,3 +25,51 @@ print(outputDateFormatter.dateFormat)
 print("From today \(outputDateFormatter.string(from: Date())) to 23/04/2017 there is \(remainingTime)")
 */
 
+let date = Date()
+print(date)
+
+let dateFormatter = DateFormatter()
+
+dateFormatter.dateStyle = .full
+dateFormatter.timeStyle = .full
+
+print(dateFormatter.string(from: date))
+
+// Compara dos fechas
+//Antes de comparar dos fechas necesitamos tenerlas como información, como dato manejable, en nuestro caso como instancias de Date
+//con las cuales podamos trabajar, así que, de paso, veamos como definir un momento en el tiempo distinto al actual:
+
+var firstDateComponents = DateComponents()
+
+firstDateComponents.day = 01
+firstDateComponents.month = 01
+firstDateComponents.year = 2017
+
+firstDateComponents.timeZone = TimeZone(abbreviation: "UTC")
+
+var firstDate = Calendar(identifier: Calendar.Identifier.gregorian).date(from: firstDateComponents)
+print(firstDate!)
+
+/** Dicho esto continuemos con nuestra comparación, añadamos otra fecha con un mes de diferencia con respecto a la primera:
+var firstDateComponents = DateComponents()
+var secondDateComponents = DateComponents()
+
+firstDateComponents.day = 01
+firstDateComponents.month = 01
+firstDateComponents.year = 2017
+
+firstDateComponents.timeZone = TimeZone(abbreviation: "UTC")
+
+secondDateComponents.day = 01
+secondDateComponents.month = 02
+secondDateComponents.year = 2017
+
+secondDateComponents.timeZone = TimeZone(abbreviation: "UTC")
+
+var firstDate = Calendar(identifier: Calendar.Identifier.gregorian).date(from: firstDateComponents)
+var secondDate = Calendar(identifier: Calendar.Identifier.gregorian).date(from: secondDateComponents)
+
+print(firstDate!)
+print(secondDate!)
+
+*/
