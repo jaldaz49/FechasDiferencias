ef1818e60a5224b7711f5f003e71a661222332b3
jaldaz49 <65065322+jaldaz49@users.noreply.github.com>
2020-05-14 21:27:20 -0300

    Update main.swift
    Copio directamente el codigo el main.swift

--- main.swift
+++ main.swift
@@ -1,5 +1,26 @@
 import Foundation
 
+var someDateComponents = DateComponents()
 
-let Ejecutor = Fechas()
-Ejecutor.Ejecuta()
+someDateComponents.day = 23
+someDateComponents.month = 04
+someDateComponents.year = 2017
+someDateComponents.calendar = Calendar(identifier: .gregorian)
+someDateComponents.timeZone = TimeZone(abbreviation: "UTC")
+
+let someDateComponentsFormatter = DateComponentsFormatter()
+
+someDateComponentsFormatter.unitsStyle = .full
+someDateComponentsFormatter.includesApproximationPhrase = true
+someDateComponentsFormatter.includesTimeRemainingPhrase = true
+someDateComponentsFormatter.allowedUnits = [.month, .day, .hour]
+
+let futureDate = someDateComponents.date
+
+let remainingTime = someDateComponentsFormatter.string(from: Date(), to: futureDate!)!
+
+let outputDateFormatter = DateFormatter()
+
+outputDateFormatter.dateFormat = "dd/MM/yyyy"
+
+print("From today \(outputDateFormatter.string(from: Date())) to 23/04/2017 there is \(remainingTime)")
